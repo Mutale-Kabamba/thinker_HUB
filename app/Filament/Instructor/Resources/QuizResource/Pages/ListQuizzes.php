6526cf6b3d68846f82a8012205808702b6f7215e
Mutale-Kabamba <190031@zcuniversity.edu.zm>
2026-04-17 01:30:22 +0200

    feat: Add ImportQuizzesAction for importing quizzes with JSON support and enhance ListQuizzes page with import functionality

--- app/Filament/Instructor/Resources/QuizResource/Pages/ListQuizzes.php
+++ app/Filament/Instructor/Resources/QuizResource/Pages/ListQuizzes.php
@@ -2,16 +2,23 @@
 
 namespace App\Filament\Instructor\Resources\QuizResource\Pages;
 
+use App\Filament\Actions\ImportQuizzesAction;
+use App\Filament\Instructor\Concerns\ScopedToInstructor;
 use App\Filament\Instructor\Resources\QuizResource\QuizResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListQuizzes extends ListRecords
 {
+    use ScopedToInstructor;
+
     protected static string $resource = QuizResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [CreateAction::make()];
+        return [
+            CreateAction::make(),
+            ImportQuizzesAction::makeForCourses(static::instructorCourseIds()),
+        ];
     }
 }
