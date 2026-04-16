04a80192dacd1e52560c029e267c0ca6d4629a08
Mutale-Kabamba <190031@zcuniversity.edu.zm>
2026-04-16 13:56:13 +0200

    feat: implement afterCreate method to trigger email verification for new users

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -4,8 +4,18 @@ namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Auth\Events\Registered;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function afterCreate(): void
+    {
+        $user = $this->record;
+
+        if (! $user->hasVerifiedEmail()) {
+            event(new Registered($user));
+        }
+    }
 }
